2bc4571acfa6c3d9f80adf21f7bdbfa67e17570b
kevinsvillaj <98902995+kevinsvillaj@users.noreply.github.com>
2026-07-06 12:45:16 -0400

    edit photo info

--- content/sidebar.js
+++ content/sidebar.js
@@ -43,7 +43,7 @@ function renderSongOfWeek() {
             <iframe 
                 width="100%" 
                 height="200" 
-                src="https://www.youtube.com/embed/${sidebarData.songOfWeek.youtubeId}" 
+                src="https://www.youtube.com/embed/${sidebarData.songOfWeek.youtubeId}?modestbranding=1&rel=0" 
                 title="YouTube video player" 
                 frameborder="0" 
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
